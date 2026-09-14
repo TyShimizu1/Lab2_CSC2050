2be8956fe4c99c7472f26e795a9bb26d5fd34657
TyShimizu1 <tjshimiz@calpoly.edu>
2026-09-14 09:19:04 -0700

    Update checkifcfile.sh

--- checkifcfile.sh
+++ checkifcfile.sh
@@ -32,37 +32,7 @@ echo "/**
 *." > "$filename" 
 
 cat temp.txt >> $filename 
-rm temp.txt 
-
-
-#!/usr/bin/env bash
-
-
-if [[ -z "$1" ]]; then
-        echo "error, only 1 argument allowed"
-        echo "Usage: $0 <filename.c">&2
-        exit 1
-fi
-
-filename=$1
-
-Owner=$(ls -l "$filename" | awk '{print $3}')
-
-last_modified=$(ls -l "$filename" | awk '{print $7, $8, $9}')
-
-if [[ "$1" != *.c ]]; then
-        echo "Not a .c file"
-        exit 1
-fi
-
-touch temp.txt
-cat $filename > temp.txt
-
-if [[ "$1" != *.c ]]; then
-        echo "Not a .c file"        
-	exit 1
-fi
-
+rm temp.txt
 echo "/**
 * File Name: $filename
 *Owner: $Owner
